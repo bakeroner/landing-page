70bca2067f3a34bacec35cfd77916ee433e30846
AM_PC <ashakhtorin@inbox.ru>
2018-09-11 16:45:21 +0300

    slider added not work gallery added not work img to gallery added

---
+++ js/gallery.js
@@ -0,0 +1,12 @@
+var slider = tns({
+    container: ".gallery",
+    prevButton: "#arrow_top",
+    nextButton: "#arrow_bot",
+    mode: "gallery",
+    items: 3,
+    axis: "horizontal",
+    speed: 400,
+    slideBy: 1,
+    controls: false,
+    loop: true
+  });
---
+++ js/slider.js
@@ -0,0 +1,12 @@
+var slider = tns({
+    container: ".slider",
+    prevButton: "#arrow_top",
+    nextButton: "#arrow_bot",
+    mode: "carousel",
+    items: 2,
+    axis: "vertical",
+    speed: 400,
+    slideBy: 1,
+    controls: false,
+    loop: true
+  });

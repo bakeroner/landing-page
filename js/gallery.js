9b82c0a424b23f39de3735927b21df213630644d
AM_PC <ashakhtorin@inbox.ru>
2018-09-15 15:55:48 +0300

    gallery script updated

--- js/gallery.js
+++ js/gallery.js
@@ -10,27 +10,29 @@
     nav: false,
   });*/
   const container = document.getElementById("gallery");
-  //const img_container = container.getElementsByClass("easy_div");
 const img_coll = container.getElementsByClassName("easy_div");
 for (let i = 0; i<img_coll.length; i++) {
   Array.from(img_coll[i].children).forEach((face) => {
       if (face.tagName == "IMG") {
-        //toHide(nod);
       face.classList.toggle("smokeElement");
     }
-    if (face.tagName == "DIV") {
-        face.classList.toggle("hideElement");
-    }
-     if (face.tagName == "P") {
+    else {
         face.classList.toggle("hideElement");
     }
     })
   }
+
     container.addEventListener("click", (event) => {
     let target = event.target;
     if (target.tagName == "IMG") {
-        target.classList.toggle("clearElement");
-        console.log(target.nextSibling);
-        /*target.nextSibling.classList.toggle("hideElement");*/
+        console.log(target.parentNode.children);
+        Array.from(target.parentNode.children).forEach((dom_element) => {
+            if (dom_element.tagName == "IMG") {
+                dom_element.classList.toggle("clearElement");
+            }
+            else {
+                dom_element.classList.toggle("hideElement");
+            }
+        })
     }
     })

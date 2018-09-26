0f3c3fc056a0f1a0ac01b80c49072bd00b25ce0e
AM_PC <ashakhtorin@inbox.ru>
2018-09-26 03:55:25 +0300

    gallery done

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,7 @@
 const gallery = document.getElementById("gallery");
 const allTargets = gallery.getElementsByClassName("member__face");
+const about = document.getElementById("about");
+const allLabels = document.getElementsByClassName("member__info");
 const focus = document.querySelector(".member__face--OnFocus");
 let currentFocus = focus;
 gallery.addEventListener("click", (event) => {
@@ -10,6 +12,15 @@ gallery.addEventListener("click", (event) => {
       target.classList.add("member__face--OnFocus");
       currentFocus.classList.remove("member__face--OnFocus");
       currentFocus.classList.add("member__face--NoFocus");
+     /* currentFocus = target;*/
+      for (let i = 0; i<allTargets.length; i++) {
+        if (target == allTargets[i]) {
+          allLabels[i].classList.remove("hideElement");
+        }
+        if (currentFocus == allTargets[i]) {
+          allLabels[i].classList.add("hideElement");
+        }
+      }
       currentFocus = target;
     }
   }

97b5d059878f0c61edf455939b4392392bc2623a
AM_PC <ashakhtorin@inbox.ru>
2018-09-26 03:30:46 +0300

    gallery still in progress

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,16 @@
 const gallery = document.getElementById("gallery");
 const allTargets = gallery.getElementsByClassName("member__face");
-const focus = document.querySelector(".member__face--on-focus");
+const focus = document.querySelector(".member__face--OnFocus");
 let currentFocus = focus;
 gallery.addEventListener("click", (event) => {
   let target = event.target;
-  if (target.classList.contains("member__face--no-focus") == true) {
-    console.log(target);
+  if (target.classList.contains("member__face--NoFocus") == true) {
     if (target != currentFocus) {
-      target.classList.remove("member__face--no-focus");
-      target.classList.add("member__face--on-focus");
-      currentFocus.classList.remove("member__face--on-focus");
-      currentFocus.classList.add("member__face--no-focus");
+      target.classList.remove("member__face--NoFocus");
+      target.classList.add("member__face--OnFocus");
+      currentFocus.classList.remove("member__face--OnFocus");
+      currentFocus.classList.add("member__face--NoFocus");
       currentFocus = target;
-      console.log(currentFocus);
     }
   }
 })

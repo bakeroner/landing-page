357ce64d0f3a192ebcfddf2202f7b852dc5b166f
AM_PC <ashakhtorin@inbox.ru>
2018-10-02 02:29:56 +0300

    mobile version added

--- js/mobile-menu_hide.js
+++ js/mobile-menu_hide.js
@@ -1,6 +1,7 @@
 const mobileMenu = document.getElementById("mobileMenu");
 const clearMenu = document.getElementById("clearMenu");
-const mobileMenuList = document.getElementById("mobileMenuList");
+const backgroundArea = document.getElementById("mobileMenuList");
+let counter = 0;
 mobileMenu.addEventListener("click", (event) => {
 	let target=event.target;
 	mobileMenuList.classList.toggle("hideElement");
@@ -13,3 +14,15 @@ clearMenu.addEventListener("click", (event) => {
 	mobileMenu.classList.toggle("hideElement");
 	clearMenu.classList.toggle("hideElement");
 })
+backgroundArea.addEventListener("click", (event) => {
+	let target=event.target;
+	if (target.tagName != "LI") {
+	mobileMenuList.classList.toggle("hideElement");
+	mobileMenu.classList.toggle("hideElement");
+	clearMenu.classList.toggle("hideElement");
+	}
+})
+
+
+
+

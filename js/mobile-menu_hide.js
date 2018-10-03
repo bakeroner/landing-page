fff0620d7d3f656a5fed606fc6bf238a9421f427
AM_PC <ashakhtorin@inbox.ru>
2018-10-03 14:58:12 +0300

    mobile menu animation added

--- js/mobile-menu_hide.js
+++ js/mobile-menu_hide.js
@@ -1,18 +1,25 @@
 const mobileMenu = document.getElementById("mobileMenu");
 const clearMenu = document.getElementById("clearMenu");
 const backgroundArea = document.getElementById("mobileMenuList");
+const header = document.querySelector(".header__navigation");
+
 let counter = 0;
 mobileMenu.addEventListener("click", (event) => {
 	let target=event.target;
-	mobileMenuList.classList.toggle("hideElement");
-	mobileMenu.classList.toggle("hideElement");
+	setTimeout(function() {
+	mobileMenu.classList.toggle("hideElement");		 
+	header.classList.toggle("navigation__link-menu--animation");
 	clearMenu.classList.toggle("hideElement");
+  }, 400);
 })
 clearMenu.addEventListener("click", (event) => {
-	let target=event.target;
-	mobileMenuList.classList.toggle("hideElement");
-	mobileMenu.classList.toggle("hideElement");
+	let target=event.target;	
+	setTimeout(function() {
 	clearMenu.classList.toggle("hideElement");
+	header.classList.toggle("navigation__link-menu--animation");
+    mobileMenu.classList.toggle("hideElement");
+	
+  }, 400);	
 })
 backgroundArea.addEventListener("click", (event) => {
 	let target=event.target;

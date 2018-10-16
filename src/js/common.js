ba791997fc3d42cd9fa1013e825b2a7b1110dbd9
AM_PC <ashakhtorin@inbox.ru>
2018-10-16 18:33:52 +0300

    looks nearly better

--- src/js/common.js
+++ src/js/common.js
@@ -6,6 +6,4 @@ import './gallery.js';
 import './mobile-menu_hide.js';
 import './slider.js';
 // import "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.8.6/min/tiny-slider.js";
-// import "https://fonts.googleapis.com/icon?family=Material+Icons";
-// import "https://use.fontawesome.com/releases/v5.3.1/css/all.css";
-// import "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.8.6/tiny-slider.css";
+import {tns} from '../../node_modules/tiny-slider/src/tiny-slider.js';

c0cd09e8b0a36598d07f4d89fbf2ae233a2eae4b
AM_PC <ashakhtorin@inbox.ru>
2018-10-18 01:48:10 +0300

    hot reload added but not used

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,32 @@
 require('./../index.html');
 import 'Styles/import.css';
 import "Styles/fonts/icofont/icofont.min.css";
+
 import './list_hide.js';
+if (module.hot) {
+  module.hot.accept('./list_hide.js', function() {
+    console.log('Accepting the updated printMe module!');
+    //printMe();
+  })
+}
 import './gallery.js';
+if (module.hot) {
+  module.hot.accept('./gallery.js', function() {
+    console.log('Accepting the updated printMe module!');
+    //printMe();
+  })
+}
 import './mobile-menu_hide.js';
+if (module.hot) {
+  module.hot.accept('./mobile-menu_hide.js', function() {
+    console.log('Accepting the updated printMe module!');
+    //printMe();
+  })
+}
 import './slider.js';
+if (module.hot) {
+  module.hot.accept('./slider.js', function() {
+    console.log('Accepting the updated printMe module!');
+    //printMe();
+  })
+}

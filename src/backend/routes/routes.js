e0e2c1a9aaa1688a66deacc689dedf788ae92610
AM_PC <ashakhtorin@inbox.ru>
2018-11-13 22:57:09 +0300

    ...

--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -1,12 +1,17 @@
 module.exports = function(app) {
 const fs = require('fs');
 const bcrypt = require('bcrypt');
-
 const userModel = require('../models/schema.js').userModel;
-/*123*/
 const mongoose = require('./../db/mongoose.js');
 mongoose.set('debug', true)
-/*123*/
+
+	app.get('/', (req, res) => {
+   		fs.readFile(__dirname + './../../html/index.html', (error, data) => {
+   			if(error) throw error;
+   			res.writeHead(200, { 'Content-Type': 'text/html' });
+    		res.end(data);
+   	});
+})
 	app.get('/signPage', (req, res) => {//when sign
    		fs.readFile(__dirname + './../../html/sign_page.html', (error, data) => {
    			if(error) throw error;
@@ -44,18 +49,6 @@ app.post('/registration', (req, res) => {
 		}
 	})
 });
-/*#########All users###########*/
-app.get('/users', (req, res) => {//all users
-	userModel.find({}, (err, users) => {
-		if (err) return next(err);
-		res.json(users);
-	});
-});
-app.get('/users/:id', (req, res, next) => {//one user
-	userModel.findById(req.params.id, (err, user) => {
-		res.json(user);
-	});
-});
 /*#################Auth#####################*/
 app.post('/login', (req, res) => {
 	let login = req.body.username;
@@ -74,4 +67,17 @@ app.post('/login', (req, res) => {
 		}
 	})
 });
+/*#########All users###########*/
+app.get('/users', (req, res) => {//all users
+	userModel.find({}, (err, users) => {
+		if (err) return next(err);
+		res.json(users);
+	});
+});
+app.get('/users/:id', (req, res, next) => {//one user
+	userModel.findById(req.params.id, (err, user) => {
+		res.json(user);
+	});
+});
+
 }
